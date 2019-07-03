321f96bdaafe789b613f1f2b4f3d0173eedc5aaf
Ken Guie <nyhunter77@yahoo.com>
2019-07-03 18:03:04 -0400

    adding tests and building gift component - tests person field

---
+++ src/components/Gift.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, ControlLabel, Button, FormControl } from 'react-bootstrap'; 
+
+class Gift extends Component {
+  constructor() {
+    super();
+
+    this.state = { person: '', present: ''};
+  }
+
+  render() {
+    return(
+      <div>
+        <Form>
+          <Form.Group>
+            <Form.Label>Person</Form.Label>
+            <Form.Control 
+              className = 'input-person'
+              onChange = {event => this.setState({ person: event.target.value })}
+            />
+          </Form.Group>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Gift;
